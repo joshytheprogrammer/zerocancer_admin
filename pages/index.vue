1260aa24e4b348e1cda3515b4ff040bf22f30bae
Joshua <jurassicjoshua@protonmail.com>
2023-11-21 17:24:09 +0100

    made changes: ''

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    Hello world
+  <div class="block mx-auto my-8">
+    <UContainer>
+      <img class="w-36 pb-4 mx-auto" src="/logo.png" alt="Zero Cancer Logo">
+      <p class="text-center py-2">This page is under development!!!</p>
+      <UButton block>
+        <NuxtLink to="/create/">Create</NuxtLink>
+      </UButton>
+    </UContainer>
   </div>
 </template>
 
