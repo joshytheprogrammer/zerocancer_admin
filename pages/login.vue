46e5aca9fb04912e6aae36034f5f119a0a0b58fb
Joshua <jurassicjoshua@protonmail.com>
2023-11-21 16:12:59 +0100

    made changes: ''

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    Email,
-    Password
+  <div class="block my-8 mx-auto py-4 max-w-lg shadow-lg rounded-lg">
+    <UContainer>
+      <img class="w-36 pb-4 mx-auto" src="/logo.png" alt="Zero Cancer Logo">
+      <UForm :state="user" class="space-y-4" :validate="validate" @submit="onSubmit">
+        <UFormGroup label="Email" name="email">
+          <UInput v-model="user.email" />
+        </UFormGroup>
+
+        <UFormGroup label="Password" name="password">
+          <UInput v-model="user.password" type="password" />
+        </UFormGroup>
+
+        <UButton :loading="loading" type="submit">
+          Submit
+        </UButton>
+      </UForm>
+  </UContainer>
   </div>
 </template>
 
@@ -10,27 +24,82 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth'
 
+const router = useRouter()
 const auth = useFirebaseAuth();
 
-const user = {
-  email: "admin@zerocancer.africa",
-  password: "NnR7#py6?:qm"
+let loading = ref(false)
+const user = reactive({
+  email: undefined,
+  password: undefined
+});
+
+function validate(user) {
+  const errors = []
+  // Validate email
+  if (!user.email) {
+    errors.push({ path: 'email', message: 'Required' });
+  } else if (!validateEmail(user.email)) {
+    errors.push({ path: 'email', message: 'Invalid email format' });
+  }
+
+  // Validate password
+  if (!user.password) {
+    errors.push({ path: 'password', message: 'Required' });
+  } else if (!validatePassword(user.password)) {
+    errors.push({ path: 'password', message: 'Password is too weak' });
+  }
+
+  return errors
 }
 
-signInWithEmailAndPassword(auth, user.email, user.password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user)
+function onSubmit() {
+  loading.value = true;
+
+  signInWithEmailAndPassword(auth, user.email, user.password)
+  .then(() => {
+    router.push('/')
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
 
     console.error({code: errorCode, message: errorMessage})
+  }).finally(() => {
+    loading.value = false;
   });
+}
+
+function validateEmail(email) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[a-zA-Z]{2,})$/;
+  return regex.test(email);
+}
+
+function validatePassword(password) {
+  // Check password length
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Check for at least one uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  // Check for at least one lowercase letter
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+
+  // Check for at least one number
+  if (!/[0-9]/.test(password)) {
+    return false;
+  }
+
+  // Check for at least one special character
+  if (!/[^a-zA-Z0-9]/.test(password)) {
+    return false;
+  }
+
+  return true;
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'zero': {
+          50: '#f4f7fb',
+          100: '#e7eef7',
+          200: '#cadbed',
+          300: '#9cbedd',
+          400: '#679bc9',
+          500: '#447fb3',
+          600: '#336699',
+          700: '#2a517a',
+          800: '#264666',
+          900: '#243c56',
+          950: '#182739'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
+
