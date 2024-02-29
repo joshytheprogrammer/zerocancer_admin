97c499f57903873ba1ae42437ea8c5f59de1db01
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-02-29 17:57:22 +0100

    made changes: ''

--- pages/create/envoy.vue
+++ pages/create/envoy.vue
@@ -1,7 +1,71 @@
 <template>
-  <UForm :state="envoy" :validate="vItem" @submit="submitEnvoy" class="space-y-3">
+   <!-- -->
+  <UForm :validate="vItem" :state="envoy" @submit="submitEnvoy" class="space-y-3">
+    <UContainer>
+      <NuxtLink class="mx-auto w-fit block" to="/">
+        <img class="w-36 pb-4 mx-auto" src="/logo.png" alt="Zero Cancer Logo">
+      </NuxtLink>
+    </UContainer>
     <UFormGroup label="Name" name="fullName">
-      <UInput v-model="envoy.fullName" />
+      <UInput v-model="envoy.fullName" placeholder="Enter the Full Name of the envoy" />
+    </UFormGroup>
+    <UFormGroup label="Title" name="title">
+      <UInput v-model="envoy.title" placeholder="Enter the envoy's title" />
+    </UFormGroup>
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="envoy.email" placeholder="Enter the envoy's email" />
+    </UFormGroup>
+    <UFormGroup label="Image Link" name="profileImage">
+      <UInput v-model="envoy.profileImage"  placeholder="Enter the image link" />
+    </UFormGroup>
+    <UFormGroup label="Specialties" name="specialties">
+      <USelectMenu
+        clear-search-on-close
+        v-model="envoy.specialties"
+        name="specialties"
+        :options="envoy.specialties"
+        multiple
+        searchable
+        creatable
+        value-attribute=""
+        show-create-option-when="always"
+        placeholder="Add specialties"
+      />
+    </UFormGroup>
+    <UFormGroup label="Memberships" name="memberships">
+      <USelectMenu
+        clear-search-on-close
+        v-model="envoy.memberships"
+        name="memberships"
+        :options="envoy.memberships"
+        multiple
+        searchable
+        creatable
+        show-create-option-when="always"
+        placeholder="Add memberships"
+      />
+    </UFormGroup>
+    <UFormGroup label="Qualifications" name="qualifications">
+      <USelectMenu
+        clear-search-on-close
+        v-model="envoy.qualifications"
+        name="qualifications"
+        :options="envoy.qualifications"
+        multiple
+        searchable
+        creatable
+        show-create-option-when="always"
+        placeholder="Add qualifications"
+      />
+    </UFormGroup>
+    <UFormGroup label="Place of Work" name="placeOfWork">
+      <UInput v-model="envoy.placeOfWork" placeholder="Enter the envoys place of work" />
+    </UFormGroup>
+    <UFormGroup label="Vision Link" name="visionLink">
+      <UInput v-model="envoy.visionLink" placeholder="Enter the link to the video vision" />
+    </UFormGroup>
+    <UFormGroup label="Vision Statement" name="visionStatement">
+      <UTextarea v-model="envoy.visionStatement" placeholder="Enter the envoys vision statement" />
     </UFormGroup>
     <UButton :loading="loading" type="submit" color="black">
       {{ loading ? 'Please wait...' : 'Add Envoy' }}
@@ -15,23 +79,25 @@ definePageMeta({
   middleware: ['auth'],
 });
 
-import { doc, setDoc } from 'firebase/firestore'
+import { collection, addDoc } from 'firebase/firestore'
 
 const db = useFirestore();
 const toast = useToast();
+const router = useRouter();
 const {validate} = useCreateUtilities();
 
 const envoy = reactive({
   fullName: undefined,
   title: undefined,
   email: undefined,
-  imgLink: undefined,
+  profileImage: undefined,
   specialties: [],
   memberships: [],
   qualifications: [],
   placeOfWork: undefined,
   visionLink: undefined,
-  visionStatement: undefined
+  visionStatement: undefined,
+  dateAdded: new Date()
 });
 
 let loading = ref(false);
@@ -39,9 +105,10 @@ let loading = ref(false);
 async function submitEnvoy() {
   loading.value = true;
 
-  await setDoc(doc(db, "envoys"), envoy)
+  await addDoc(collection(db, "envoys"), envoy)
   .then(() => {
     toast.add({ title: 'Envoy added successfully' })
+    router.push('/')
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -55,7 +122,7 @@ async function submitEnvoy() {
 }
 
 function vItem() {
-  return validate(centre)
+  return validate(envoy)
 }
 
 </script>
